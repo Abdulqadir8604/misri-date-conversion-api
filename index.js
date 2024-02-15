c893605043772afdd2b57a7aaa897cae68d920a6
Abdulqadir8604 <web.aq8604@gmail.com>
2024-02-15 22:20:22 +0530

    added cors policy

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 const app = require('express')();
+const cors = require('cors');
 const main = require('./main');
 
 const port = 8080;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use(cors());
 
-app.get('/gregToMisri/:gDate', (req, res) => {
-    const gDate = req.params.gDate;
+app.get('/gregToMisri/:date', (req, res) => {
+    const gDate = req.params.date;
     const dateParts = gDate.split("-")
     const day = dateParts[0].padStart(2, '0')
     const month = dateParts[1].padStart(2, '0')
     const year = dateParts[2]
     const mDate = main.GregToMisri(day, month, year);
-    res.status(200).json({ 
+    res.status(200).json({
         message: 'Gregorian to Misri',
         data: mDate
-    }); 
+    });
 });
 
-app.get('/misriToGreg/:mDate', (req, res) => {
-    const mDate = req.params.mDate;
+app.get('/misriToGreg/:date', (req, res) => {
+    const mDate = req.params.date;
     const dateParts = mDate.split("-")
     const day = dateParts[0].padStart(2, '0')
     const month = dateParts[1].padStart(2, '0')
     const year = dateParts[2]
     const gDate = main.MisriToGreg(day, month, year);
-    res.status(200).json({ 
+    res.status(200).json({
         message: 'Misri to Gregorian',
         data: gDate
-    }); 
+    });
 });
 
 
-  
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+});
+
